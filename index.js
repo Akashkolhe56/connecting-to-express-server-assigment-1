8edfd6886b2a206fd8c7b23bd5e4f99efb87b5fc
Akashkolhe56 <akashkolhe089@gmail.com>
2025-01-31 13:48:56 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+require('dotenv').config()
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose = require('mongoose');
+const { error } = require('console');
 const app = express();
-const port = 3010;
+const port = 3010; 
+const DB_URL=process.env.URL
 
 app.use(express.static('static'));
-
+const connectDB=async ()=>{
+  await mongoose.connect(DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    console.log("Connected to MongoDB")
+  }).catch((error)=>{
+    console.log(error)
+  })
+}
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
+connectDB().then(()=>{
+  app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+})
+
